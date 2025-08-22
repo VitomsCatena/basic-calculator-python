57b740e73fe47b5a50c53477027ccc257787198b
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2025-08-22 10:45:24 +0300

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
-
+def calculate(num1, num2, operation):
+    """
+    Performing a mathematical operation on two numbers.
+    """
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        # Handle division by zero
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation."
